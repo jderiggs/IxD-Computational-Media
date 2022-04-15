f6488d0d2678c14a815eac2a2e0b629743d9a67e
jderiggs <jaedaggz@gmail.com>
2022-04-14 22:56:20 -0400

    Hamburger menu animation

---
+++ main.js
@@ -0,0 +1,5 @@
+const menuBtn= document.querySelector('.menu-btn'); //creating a constant statement targeting the menu-btn class
+let menuOpen = false //declaring that the menu should be closed globally
+menuBtn.addEventListener('click', () => { //allowing the menu button to be interacted with when clicked/tapped on
+    menuBtn.classList.toggle('open');
+});//when the menu button is clicked/tapped, toggles the class 'open'
