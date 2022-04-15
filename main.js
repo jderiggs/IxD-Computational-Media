a87ce4dc6fae3cfa035c7d0e0344b4987b90e399
jderiggs <jaedaggz@gmail.com>
2022-04-15 00:07:10 -0400

    Added gradient to titlebar, and comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Animation for changing the hamburger menu to a closed icon
 const menuBtn= document.querySelector('.menu-btn'); //creating a constant statement targeting the menu-btn class
 let menuOpen = false //declaring that the menu should be closed globally
 menuBtn.addEventListener('click', () => { //allowing the menu button to be interacted with when clicked/tapped on
