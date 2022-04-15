a90d630e7c611b7adeb68e82d9390bdc70c24964
jderiggs <jaedaggz@gmail.com>
2022-04-15 01:37:03 -0400

    Implemented navbar functionality

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 //Animation for changing the hamburger menu to a closed icon
 const menuBtn= document.querySelector('.menu-btn'); //creating a constant statement targeting the menu-btn class
+const nav=document.querySelector('nav'); //creating a constant statement targeting the nav
 let menuOpen = false //declaring that the menu should be closed globally
 menuBtn.addEventListener('click', () => { //allowing the menu button to be interacted with when clicked/tapped on
     menuBtn.classList.toggle('open');
-});//when the menu button is clicked/tapped, toggles the class 'open'
+    nav.classList.toggle('active');
+});//when the menu button is clicked/tapped, toggles the class 'open' and sets the nav to 'active'
